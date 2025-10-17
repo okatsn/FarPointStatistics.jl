4183fcbc22435181a205fa639c5b2f6c098db5f0
Documenter.jl <documenter@juliadocs.github.io>
2025-10-17 01:55:06 +0000

    build based on 2629439

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FarPointStatistics","page":"Home","title":"FarPointStatistics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FarPointStatistics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
